c5a49ae7d9a839e7ea5fb929331350b1b47899f6
shravani866 <shravaniumanabadi9@gmail.com>
2025-09-13 00:49:12 +0530

    Add files via upload

---
+++ Hourglass pattern.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i,j,space;
+    printf("Enter the number of rows:");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+        for(space=0;space<n-i;space++)
+        {
+            printf(" ");
+        }
+        for(j=0;j<2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(i=2;i<=n;i++)
+    {
+        for(space=0;space<n-i;space++)
+        {
+            printf(" ");
+        }
+        for(j=0;j<2*i-1;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
