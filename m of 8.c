f993e178384dbc97ee7c992787ddb862c15cb85e
shravani866 <shravaniumanabadi9@gmail.com>
2025-09-14 23:03:02 +0530

    printing multiple of 8

---
+++ m of 8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        if(i%8==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+}
+
