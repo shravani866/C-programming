c3eba9e03c976f672da37279a3b6a68f0ec54705
shravani866 <shravaniumanabadi9@gmail.com>
2025-09-14 23:01:46 +0530

    Add files via upload

---
+++ Multiflies of 6table.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        if(i%6==0)
+        {
+            printf("%d\n",i);
+        }
+    }
+}
+
