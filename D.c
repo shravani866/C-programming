35aee5c30bd7713fb47085a8f7cce4407750177b
shravani866 <shravaniumanabadi9@gmail.com>
2025-09-16 23:38:13 +0530

    printing alphabet "D" using (*).

---
+++ D.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(j==2||i==1||i==5||j==5)
+               printf("*");
+            else
+               printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
