ffe3d334884362cf7bcb4fb52f09ed0d6a45d0c0
shravani866 <shravaniumanabadi9@gmail.com>
2025-09-16 23:43:19 +0530

    printing alphabet "M" using (*).

---
+++ M.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(j==1||j==5||i==j&&j<=3||i+j==6&&j>=3)
+               printf("*");
+            else
+               printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
