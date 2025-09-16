44b762f0013810cb4c8605547edb14c5e263bba0
shravani866 <shravaniumanabadi9@gmail.com>
2025-09-16 23:40:24 +0530

    printing alphabet "H" using (*).

---
+++ H.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(j==1||j==5||i==3)
+               printf("*");
+            else
+               printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
