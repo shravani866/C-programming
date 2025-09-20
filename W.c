09542d190bd904560eee53b1340cc1bd1922a092
shravani866 <shravaniumanabadi9@gmail.com>
2025-09-20 18:12:27 +0530

    Printing alphabet "W" using (*).

---
+++ W.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(j==1||j==5||(i==j&&j>=3)||(i+j==6&&j<=3))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
