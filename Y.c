f24568bbf9f3e8b3d41652233ac234c73658a567
shravani866 <shravaniumanabadi9@gmail.com>
2025-09-20 18:23:12 +0530

    Printing alphabet "Y" using (*).

---
+++ Y.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if((i==j&&j<=3||i+j==6&&j>=3||j==3&&i>=3))
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
